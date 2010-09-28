ef07f11973a51f6c698d40e0842973b985528a80
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2010-09-27 18:29:17 -0700

    Adding setup.py to make it installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# FIXME add boto dependency, or split the package in different projects, the core and the APIs
+
+from distutils.core import setup
+
+setup(
+    name='cloud-tool',
+    version='0.0.1',
+    description='Command-line tool to manage computing clouds',
+    author='Manuel Amador',
+    author_email='rudd-o@rudd-o.com',
+    url='http://github.com/Rudd-O/cloud-tool',
+    packages=['cloudtool'],
+    scripts=['bin/cloud-tool'],
+)
